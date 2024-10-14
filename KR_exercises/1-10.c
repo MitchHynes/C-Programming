31092ff75664540f8bff291a92973f77e1b3c365
Mitchell Hynes <mpeterhynes@gmail.com>
2024-10-14 01:22:27 -0400

    adds 1-10.c

--- KR_exercises/1-10.c
+++ KR_exercises/1-10.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
 int main() {
-	int a = EOF;
-	printf("a: %d\n",a);
+	int c;
+	while ((c = getchar()) != EOF) {
+		if (c == '\t') {//using '' (single quotes) means the character represents the integer value of that character
+			putchar('\\');
+			putchar('t');
+		} else if (c == '\b') { //seems like the stdin to the terminal doesn't record this action?
+			putchar('\\');
+			putchar('b');
+		} else if (c == '\\') {
+			putchar('\\');
+			putchar('\\');
+		}
+		else
+			putchar(c);
+	}
 	return 0;
 }
