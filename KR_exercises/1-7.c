3ce5d498b0a93c1ede4ca42ccff2b269fda90023
Mitchell Hynes <mpeterhynes@gmail.com>
2024-10-13 23:46:52 -0400

    initial commit

---
+++ KR_exercises/1-7.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	int a = EOF; //EOF is an int value, assign it to a variable
+	printf("a: %d\n",a); //printf is a general purpose output formatting function.
+	return 0;
+}
