57ea59801b622d7b101490576bf38dc936a538eb
Mitchell Hynes <mpeterhynes@gmail.com>
2024-10-23 19:31:49 -0400

    adds 1-19.c. Remember git operator assignment precedence

---
+++ KR_exercises/1-19.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define MAXLINE 1000 //maximum input line size
+
+int n_getline(char s[], int lim);
+
+int n_getline(char s[], int lim) {
+	int c, i;
+	for (i = 0; (i < lim - 1) && ((c=getchar()) != EOF) && (c != '\n') ;i++) {
+		s[i] = c;	
+
+	} //this will capture all the way up to a \n
+	if (c == '\n') {
+		s[i] = '\n';
+		++i;
+	}
+	s[i] = '\0';
+	return i-1; //subtract one to be less than the ending character in the return
+}
+
+int main() {
+	
+	char line[MAXLINE];
+	int len = n_getline(line, MAXLINE);
+	int temp = len - 1;
+	int i = 0;
+	while (temp > i) {
+		int b = line[temp];
+		line[temp] = line[i];
+		line[i] = b;
+		--temp;
+		++i;
+	}
+	printf("%s",line);
+}
+
