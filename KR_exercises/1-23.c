4ac420aa4d8ffb2d9866f9bb77885d0475a6645d
Mitchell Hynes <mpeterhynes@gmail.com>
2024-11-15 23:37:38 -0500

    adds function declarations and main

--- KR_exercises/1-23.c
+++ KR_exercises/1-23.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+void handle_slash(char *previous, int *in_comment, int *in_quote);
+void handle_asterisk(char *previous, int *in_comment, int *in_quote);
+void handle_quote(char *previous, int *in_comment, int *in_quote);
 
 //need to think about the quotes
 //try drawing out a full state machine for what each case would be and how to handle. Maybe make a table?
 //look for anywhere that can be modularized into functions
+void handle_slash(char *previous, int *in_comment, int *in_quote) {
+	if (*in_quote) {
+		putchar('/');
+	} else if (*in_comment && (*previous == '*')) {
+		*in_comment = 0;
+	}
+}
+void handle_asterisk(char *previous, *int in_comment, int *in_quote) {
+
+}
+void handle_quote(char *previous, int *in_comment, int *in_quote) {
+	if (*in_quote) {
+		*in_quote = 0;
+	} else {
+		*in_quote = 1;
+	}
+	putchar('"');
+
+}
+
 int main()
 {
 	int in_comment = 0;
@@ -22,11 +45,20 @@ loop through all the characters while not EOF
 
 */
 
-	printf("testing \"""comment /* haha */");
-	while ((c = getchar()) != EOF)
-	{
+
+	while ((c = getchar()) != EOF) {
+		switch(c) {
+		case '/':
+			handle_slash(&previous, &in_comment, &in_quote);
+		case '*':
+			handle_asterisk(&previous, &in_comment, &in_quote);
+		case '"':
+			handle_quote(&previous, &in_comment, &in_quote);
+		default:
+			putchar(c);
+			previous = c;
+		}
 		
 	}
-
 	return 0;
 }
